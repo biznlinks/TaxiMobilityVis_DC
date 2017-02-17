de72f107b77eefdd0ff1d2e3512c4dbb42240416
alephcero <4lephcero@gmail.com>
2017-02-17 18:05:13 -0500

    basemap ready

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # mobilityVis
 The purpose of this project is to come up with a visualization platform for a travel demand modeling
 
-* [website](https://alephcero.github.io/mobilityVis/)  
+* [Test site](https://alephcero.github.io/mobilityVis/) 
+* [Leaflet site](http://leafletjs.com/)
+* [D3 site](https://d3js.org/)
+* [Tutorial on d3 movement](http://zevross.com/blog/2014/09/30/use-the-amazing-d3-library-to-animate-a-path-on-a-leaflet-map/)
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,2 @@
 // instantiate a leaflet map into my container
 // set the initial view
-var map = L.map('mapContainer').setView([-34.602889, -58.442420], 12);
-
-// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-}).addTo(map);
