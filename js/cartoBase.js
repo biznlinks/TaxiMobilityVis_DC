b317795ce20788b32222076a94bc22d62188f2ff
alephcero <4lephcero@gmail.com>
2017-02-18 18:14:36 -0500

    first attemp to implement d3 animation failed

---
+++ js/cartoBase.js
@@ -0,0 +1,4 @@
+    var map = L.map('mapContainer').setView([40.735021, -73.994787], 12);
+    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+    }).addTo(map);
